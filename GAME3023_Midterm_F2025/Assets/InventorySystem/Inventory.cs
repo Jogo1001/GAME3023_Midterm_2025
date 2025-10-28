dd6fff44ce5f3ef0fbab4893f19537dcfe18a914
Jogo1001 <101247726@georgebrown.ca>
2025-10-28 12:53:01 -0400

    grid setup function

--- GAME3023_Midterm_F2025/Assets/InventorySystem/Inventory.cs
+++ GAME3023_Midterm_F2025/Assets/InventorySystem/Inventory.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Inventory : MonoBehaviour
 {
-    List<ItemSlot> itemSlots = new List<ItemSlot>();
+    [Header("Inventory Grid Settings")]
+    [SerializeField] private GameObject inventoryPanel; 
+    [SerializeField] private int columns = 6;
+    [SerializeField] private int rows = 5;
+    [SerializeField] private float cellSize = 80f;
+    [SerializeField] private float spacing = 5f;
 
-    [SerializeField]
-    GameObject inventoryPanel; // grid
+    private List<ItemSlot> itemSlots = new();
+    private RectTransform gridRect;
 
     void Start()
     {
-        //Read all itemSlots as children of inventory panel
         itemSlots = new List<ItemSlot>(
-            inventoryPanel.transform.GetComponentsInChildren<ItemSlot>()
-            );
+           inventoryPanel.GetComponentsInChildren<ItemSlot>()
+       );
+
+        gridRect = inventoryPanel.GetComponent<RectTransform>();
+        SetupGridLayout();
+    }
+    private void SetupGridLayout()
+    {
+        if (inventoryPanel.TryGetComponent(out GridLayoutGroup grid))
+        {
+            grid.cellSize = new Vector2(cellSize, cellSize);
+            grid.spacing = new Vector2(spacing, spacing);
+            grid.constraint = GridLayoutGroup.Constraint.FixedColumnCount;
+            grid.constraintCount = columns;
+        }
     }
 }
